8e9230e9fa561b0a02205086255a518082910a67
Aranza <cybergoth2077@outlook.com>
2025-05-18 15:57:18 -0600

    PAREN TODO, YA FUNCIONÓ

---
+++ registro/src/main/java/com/tolerante/registro/RegistroApplication.java
@@ -0,0 +1,15 @@
+package com.tolerante.registro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RegistroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroApplication.class, args);
+	}
+
+}
